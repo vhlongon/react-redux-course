e98719a6b93db36b9c4ff99ec13df1268e39442a
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-03-06 20:55:03 +0100

    add a test component and render on index.html

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Component} from 'react';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
