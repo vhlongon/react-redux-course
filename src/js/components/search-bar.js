c41078c6a9a640edf0e8d0b007bcc88169a47055
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-03-14 21:44:35 +0100

    create search export and import it on app file

---
+++ src/js/components/search-bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+  return (
+    <input />
+  );
+}
+
+export default SearchBar;
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import SearchBar from './components/search-bar';
+
 import css from '../scss/styles.scss';
 
+const API_KEY = 'AIzaSyAkR_KUryY_eCDzpRGYIWEvYpI2yI2rSmc';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 ReactDOM.render(
   <App />,
   document.querySelector('.react-root')
