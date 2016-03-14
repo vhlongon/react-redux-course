d09cf71d87edbe40f99c632f2a6e445dbae258bc
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-03-14 22:18:42 +0100

    add on change event handler

--- src/js/components/search-bar.js
+++ src/js/components/search-bar.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = (props) => {
-  return (
-    <input />
-  );
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  render = () => {
+    return (
+      <input onChange={this.onInputChange} />
+    );
+  }
 }
 
 export default SearchBar;
